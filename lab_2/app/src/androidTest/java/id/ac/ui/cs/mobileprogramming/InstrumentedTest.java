5aab4f5bf03d7e60ee60161db5a87439bfa1df97
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-09-28 18:26:39 +0700

    fixed instrumented tests & Unit tests

--- lab_2/app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/InstrumentedTest.java
+++ lab_2/app/src/androidTest/java/id/ac/ui/cs/mobileprogramming/InstrumentedTest.java
@@ -1,12 +1,20 @@
 package id.ac.ui.cs.mobileprogramming;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,8 +23,10 @@ import org.junit.runner.RunWith;
 import id.ac.ui.cs.mobileprogramming.MainActivity;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -30,16 +40,44 @@ import static org.junit.Assert.assertThat;
 @RunWith(AndroidJUnit4.class)
 public class InstrumentedTest {
 
-    //MainActivity ma = new MainActivity();
+    Matcher<View> hasValueEqualTo(final String content) {
+
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Has EditText/TextView the value:  " + content);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextView) && !(view instanceof EditText)) {
+                    return false;
+                }
+                if (view != null) {
+                    String text;
+                    if (view instanceof TextView) {
+                        text = ((TextView) view).getText().toString();
+                    } else {
+                        text = ((EditText) view).getText().toString();
+                    }
+
+                    return (text.equalsIgnoreCase(content));
+                }
+                return false;
+            }
+        };
+    }
+
 
     @Rule
     public ActivityScenarioRule<MainActivity> asr = new ActivityScenarioRule<MainActivity>(MainActivity.class);
 
-    // private MainActivity mainActivity = null;
 
     @Test
-    public void helloWorldTextTest() {
-        onView(withText("Hello World by 1706040031!")).check(matches(isDisplayed()));
+    public void screenTextTest() {
+        onView(withText("Random number between 1 and 100:")).check(matches(isDisplayed()));
+        onView(withText("Random number app by 1706040031!")).check(matches(isDisplayed()));
     }
 
 
@@ -49,4 +87,14 @@ public class InstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("id.ac.ui.cs.mobileprogramming", appContext.getPackageName());
     }
+
+    @Test
+    public void beforeButtonPressed() {
+
+        onView(withId(R.id.screen_number)).check(matches(hasValueEqualTo("0")));
+        // onView(withId(R.id.screen_number)).check(matches(hasValueEqualTo("10"))); --> fail
+    }
+
+
+
 }
