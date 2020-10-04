ed10259e1adb45d93f95d429680a3e421e57bb0f
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-10-04 19:09:18 +0700

    exit button now removes app from recent apps

--- lab-3/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
+++ lab-3/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -2,11 +2,14 @@ package id.ac.ui.cs.mobileprogramming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
     private long pauseOffset;
     private boolean isRunning;
     public Button startButton;
+    public Button exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         chronometer = findViewById(R.id.chronometer);
     }
 
@@ -31,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             chronometer.start();
             isRunning = true;
-            // startButton.setText("START");
             startButton.setVisibility(View.INVISIBLE);
         }
 
@@ -55,5 +57,32 @@ public class MainActivity extends AppCompatActivity {
         startButton.setText("START");
     }
 
+    @Override
+    public void onBackPressed() {
+        Toast toast = Toast.makeText(
+                getApplicationContext(),
+                "Press EXIT to exit this app!",
+                Toast.LENGTH_SHORT
+        );
+        toast.show();
+    }
 
+
+    public void exitApp(View view) {
+        this.finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    // Remove dari recent apps
+    @Override
+    public void finish() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            super.finishAndRemoveTask();
+        } else {
+            super.finish();
+        }
+    }
 }
