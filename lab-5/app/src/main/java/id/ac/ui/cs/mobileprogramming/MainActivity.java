ab487bd316548bb6dc6a2a8c7626bd4d82041727
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-11-28 15:10:16 +0700

    initial commit

---
+++ lab-5/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -0,0 +1,71 @@
+package id.ac.ui.cs.mobileprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String test = getCurrentSsid(MainActivity.this);
+        Log.i(TAG, test);
+        
+
+    }
+
+    public String getCurrentSsid(Context context) {
+
+        String ssid = null;
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (networkInfo.isConnected()) {
+            final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+            if (connectionInfo != null && !(connectionInfo.getSSID().equals(""))) {
+                //if (connectionInfo != null && !StringUtil.isBlank(connectionInfo.getSSID())) {
+                ssid = connectionInfo.getSSID();
+            }
+            // Get WiFi status MARAKANA
+            WifiInfo info = wifiManager.getConnectionInfo();
+            String textStatus = "";
+            textStatus += "\n\nWiFi Status: " + info.toString();
+            String BSSID = info.getBSSID();
+            String MAC = info.getMacAddress();
+
+            List<ScanResult> results = wifiManager.getScanResults();
+            ScanResult bestSignal = null;
+            int count = 1;
+            String etWifiList = "";
+            for (ScanResult result : results) {
+                etWifiList += count++ + ". " + result.SSID + " : " + result.level + "\n" +
+                        result.BSSID + "\n" + result.capabilities + "\n" +
+                        "\n=======================\n";
+            }
+            Log.v(TAG, "from SO: \n" + etWifiList);
+
+
+        }
+        return ssid;
+    }
+}
+
+
+// https://stackoverflow.com/questions/3531940/how-to-get-name-of-wifi-network-out-of-android-using-android-api
