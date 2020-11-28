1c9c49d2486508e7d3e538282b7a8634ffe36317
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-11-28 16:35:27 +0700

    changed dependencies to androidX

--- lab-5/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
+++ lab-5/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -1,71 +1,110 @@
 package id.ac.ui.cs.mobileprogramming;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.os.Bundle;
-import android.util.Log;
+import android.os.Build;
 
-import java.util.List;
+
+import android.support.annotation.NonNull;
+
+//import android.support.v4.app.ActivityCompat;
+import androidx.core.app.ActivityCompat;
+
+import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
+
+
+import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getName();
-
+    private ListView wifiList;
+    private WifiManager wifiManager;
+    private final int MY_PERMISSIONS_ACCESS_COARSE_LOCATION = 1;
+    WifiReceiver receiverWifi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String test = getCurrentSsid(MainActivity.this);
-        Log.i(TAG, test);
-        
-
-    }
-
-    public String getCurrentSsid(Context context) {
-
-        String ssid = null;
-        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (networkInfo.isConnected()) {
-            final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
-            if (connectionInfo != null && !(connectionInfo.getSSID().equals(""))) {
-                //if (connectionInfo != null && !StringUtil.isBlank(connectionInfo.getSSID())) {
-                ssid = connectionInfo.getSSID();
-            }
-            // Get WiFi status MARAKANA
-            WifiInfo info = wifiManager.getConnectionInfo();
-            String textStatus = "";
-            textStatus += "\n\nWiFi Status: " + info.toString();
-            String BSSID = info.getBSSID();
-            String MAC = info.getMacAddress();
-
-            List<ScanResult> results = wifiManager.getScanResults();
-            ScanResult bestSignal = null;
-            int count = 1;
-            String etWifiList = "";
-            for (ScanResult result : results) {
-                etWifiList += count++ + ". " + result.SSID + " : " + result.level + "\n" +
-                        result.BSSID + "\n" + result.capabilities + "\n" +
-                        "\n=======================\n";
-            }
-            Log.v(TAG, "from SO: \n" + etWifiList);
-
-
+        wifiList = findViewById(R.id.wifiList);
+        Button buttonScan = findViewById(R.id.scanBtn);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(getApplicationContext(), "Turning WiFi ON...", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
         }
-        return ssid;
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
+                } else {
+                    wifiManager.startScan();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        receiverWifi = new WifiReceiver(wifiManager, wifiList);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(receiverWifi, intentFilter);
+        getWifi();
+    }
+    private void getWifi() {
+        if (Build.VERSION.SDK_INT > = Build.VERSION_CODES.M) {
+            Toast.makeText(MainActivity.this, "version> = marshmallow", Toast.LENGTH_SHORT).show();
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "location turned off", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
+            } else {
+                Toast.makeText(MainActivity.this, "location turned on", Toast.LENGTH_SHORT).show();
+                wifiManager.startScan();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "scanning", Toast.LENGTH_SHORT).show();
+            wifiManager.startScan();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiverWifi);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] = = PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "permission granted", Toast.LENGTH_SHORT).show();
+                wifiManager.startScan();
+            } else {
+                Toast.makeText(MainActivity.this, "permission not granted", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            break;
+        }
     }
 }
 
-
 // https://stackoverflow.com/questions/3531940/how-to-get-name-of-wifi-network-out-of-android-using-android-api
+// https://stackoverflow.com/questions/20741142/android-list-wifi-access-points
+// https://stackoverflow.com/questions/32742327/neither-user-10102-nor-current-process-has-android-permission-read-phone-state
+// https://stackoverflow.com/questions/18741034/how-to-get-available-wifi-networks-and-display-them-in-a-list-in-android
+// https://www.tutorialspoint.com/how-to-get-available-wifi-networks-and-display-them-in-a-list-in-android
