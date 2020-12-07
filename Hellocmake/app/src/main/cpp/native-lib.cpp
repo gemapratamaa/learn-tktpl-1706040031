e84ddd1056011938b633f2eb9e01649063a31a83
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-12-07 13:09:54 +0700

    added new project for experiment

---
+++ Hellocmake/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,31 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_id_ac_ui_cs_hello_1cmake_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}extern "C"
+
+
+JNIEXPORT jint JNICALL
+Java_id_ac_ui_cs_hello_1cmake_MainActivity_randomIntFromJNI(JNIEnv *env, jobject thiz) {
+    srand ( time(NULL) );
+    int number_array[] = {1, 2};
+    int random_index = rand() % 2;
+    return number_array[random_index];
+}extern "C"
+
+JNIEXPORT jstring JNICALL
+Java_id_ac_ui_cs_hello_1cmake_MainActivity_helloWorldFromJNI(JNIEnv *env, jobject thiz, jint x) {
+    std::string hello = "Hello, user!";
+    int native_x = (int) x;
+    if (native_x == 1) {
+        hello = "Hello, world!";
+    }
+
+    return env->NewStringUTF(hello.c_str());
+
+}
--- lab-6/app/src/main/cpp/Lab6.cpp
+++ lab-6/app/src/main/cpp/Lab6.cpp
@@ -2,4 +2,3 @@
 // Created by gemap on 05-Dec-20.
 //
 
-aaa
