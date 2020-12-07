cedc4be62678bd5d836ba89555e88fac928fc605
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-12-07 13:31:04 +0700

    attempt to replace existing files with successful ones

--- lab-6/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
+++ lab-6/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -2,24 +2,31 @@ package id.ac.ui.cs.mobileprogramming;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "Lab 2";
+    private static final String TAG = "Lab 1";
+    static {
+        System.loadLibrary("native-lib");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.random_button);
-        final TextView initialNumber = findViewById(R.id.screen_number);
+        TextView tv = findViewById(R.id.hello);
 
+        int randomInt = randomIntFromJNI();
+        Log.i("main","random int: " + randomInt);
+        String text = helloWorldFromJNI(randomInt);
+        tv.setText(text);
+
+        /*
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +35,15 @@ public class MainActivity extends AppCompatActivity {
                 initialNumber.setText(rn.getRandomNumber());
             }
         });
+
+         */
     }
+
+    public native String stringFromJNI();
+
+    public native int randomIntFromJNI();
+
+    public native String helloWorldFromJNI(int x);
 
 
 }
