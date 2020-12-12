4bba7af7df6bd7ce6bfe4edfc3e3334c832e45aa
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-12-12 11:20:56 +0700

    added hello world text

--- Lab7/app/src/main/cpp/native-lib.cpp
+++ Lab7/app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_id_ac_ui_cs_mobileprogramming_MainActivity_stringFromJNI(
+Java_id_ac_ui_cs_mobileprogramming_MainActivity_helloWorldFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello World by 1706040031!";
     return env->NewStringUTF(hello.c_str());
 }
