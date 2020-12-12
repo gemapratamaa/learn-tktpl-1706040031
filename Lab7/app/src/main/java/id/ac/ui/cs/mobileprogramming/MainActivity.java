1225ab008864494c5639c84a0df71dbda305bdbe
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-12-12 13:30:17 +0700

    changed native to regular android project, and to match cross platform app

--- Lab7/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
+++ Lab7/app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -2,29 +2,17 @@ package id.ac.ui.cs.mobileprogramming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(helloWorldFromJNI());
+        TextView text = findViewById(R.id.hello_world);
+        text.setTextColor(Color.parseColor("#00000"));
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String helloWorldFromJNI();
 }
